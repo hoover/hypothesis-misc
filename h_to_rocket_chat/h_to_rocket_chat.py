1993e6ed8b0d2d719a3986d7a0aa1cae6f88faed
Dan <dan@hostatic.ro>
2016-09-22 15:59:22 +0300

    Initial commit

---
+++ h_to_rocket_chat/h_to_rocket_chat.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python3.4
+import requests
+import json
+import configparser
+import redis
+import time
+import argparse
+
+# Find out which config file we want to read
+parser = argparse.ArgumentParser()
+parser.add_argument('--config', help='Read the configuration from this file',
+                    default='/etc/h_to_rocket_chat.conf')
+args = parser.parse_args()
+config_file = args.config
+
+# Read the config file
+try:
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    redis_host = config['redis']['host']
+    redis_port = config['redis']['port']
+    redis_db = config['redis']['database']
+    hypothesis_url = config['hypothesis']['url']
+    hypothesis_search_params = config['hypothesis']['search_params']
+    rocketchat_endpoint = config['rocketchat']['endpoint']
+    rocketchat_path = config['rocketchat']['path']
+    rocketchat_auth_token = config['rocketchat']['auth_token']
+except Exception as e:
+    print('Can not read config file: %s' % e)
+    exit(3)
+
+# Connnect to the Redis database, which will be used to store the IDs and links
+# of the annotations we have already sent to the Rocket chat channel
+try:
+    r = redis.StrictRedis(host=redis_host, port=redis_port, db=redis_db)
+except Exception as e:
+    print('Can not connect to Redis: %s' % e)
+    exit(3)
+
+# This function will take the various pieces of information from an annotation
+# and construct a message and post it to a Rocket chat channel
+def post_to_rocket_chat(rc_endpoint,rc_path,rc_token,message,title,link,
+                        text,source):
+    rc_headers = {'Authorization': 'Bearer ' + rc_token,
+                 'Content-Type': 'application/x-www-form-urlencoded'}
+    rc_payload = 'payload={"text":"%s", "attachments":[{"title":"%s",\
+                  "title_link":"%s", "text":"Source: %s. %s",\
+                  "color":"#764FA5"}]}' % (message, title, link, source, text)
+    rc_request = requests.post(rc_endpoint + rc_path, headers=rc_headers,
+                  data=rc_payload.encode('utf-8'))
+
+# Check if the annotation ID is in Redis a.k.a. has already been sent to chat
+def check_id_in_redis(annotation_id):
+    if r.get(annotation_id) == None: return False
+    else: return True
+
+# Call the Hypothesis API endpoint and get annotations based on the search
+# term found in the config file
+def get_annotations():
+    h_search = hypothesis_search_params
+    h_request = requests.get(hypothesis_url + h_search)
+    h_result = json.loads(h_request.text)
+    return h_result
+
+# Get the annotations, extract the info we need from them, store in Redis and
+# post them to Rocket chat
+def main():
+    try:
+        annotations = get_annotations()
+    except Exception as e:
+        print('Can not get the annotations from Hypothesis: %s' % e)
+        exit(3)
+    for row in annotations['rows']:
+        h_id = row['id']
+        if 'title' in row['document']: h_title = row['document']['title'][0]
+        else: h_title = '<No title>'
+        h_text = row['text']
+        h_source = row['uri']
+        h_link = row['links']['html']
+        h_user = row['user'].split(':')[1].split('@')[0]
+        h_tags = " ".join([str(x) for x in row['tags']] )
+        if check_id_in_redis(h_id) == False:
+            try:
+                r.set(h_id, h_link)
+            except Exception as e:
+                print('Can not write the annotation ID to Redis: %s' % e)
+                exit(3)
+            message = "@%s created a new annotation. Tags: %s" % (h_user,h_tags)
+            try:
+                post_to_rocket_chat(rocketchat_endpoint,rocketchat_path,
+                                    rocketchat_auth_token,message,h_title,
+                                    h_link,h_text,h_source)
+            except Exception as e:
+                print('Can not post to Rocket chat: %s' % e)
+                exit(3)
+
+# We want to run this script under supervisor, so go run through the main loop
+# endlessly, with a short break and let supervisor take care of the rest
+if __name__ == '__main__':
+    while True:
+        main()
+        time.sleep(3)
---
+++ sync_pi_users/sync_pi_users.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3.4
+import requests
+import json
+import subprocess
+import string
+import random
+import configparser
+import redis
+
+# Read the config file
+config = configparser.ConfigParser()
+config.read('/etc/sync_pi_users.conf')
+url = config['privacyidea']['url']
+realm = config['privacyidea']['realm']
+user = config['privacyidea']['user']
+password = config['privacyidea']['password']
+redis_host = config['redis']['host']
+redis_port = config['redis']['port']
+
+# Connect to Redis to read and store a cache of already added users to H
+r = redis.StrictRedis(host=redis_host, port=redis_port)
+users_in_redis = json.loads(r.get('h_users').decode('utf-8'))
+
+# Function to generate random passwords for users
+def generate_password(charsize):
+    chars = string.ascii_letters + string.digits
+    pwdSize = charsize
+    return ''.join((random.choice(chars)) for x in range(pwdSize))
+
+# Get an authentication token from Privacy Idea. This is usually valid for 1h
+auth_payload = {'username': user, 'password': password}
+auth_r = requests.post(url + '/auth', data=auth_payload)
+auth_result = json.loads(auth_r.text)
+token = auth_result['result']['value']['token']
+
+# Use the previous auth token in a API request to PI to get a list of users
+headers = {'Accept': 'application/json', 'Authorization': token}
+user_r = requests.get(url + '/user?realm=' + realm, headers=headers)
+users_response = json.loads(user_r.text)
+users = users_response['result']['value']
+
+# Go through the list of users and add them to Hypothesis unless they have
+# been already added to it. We check that by comparing to the Redis cache
+for user in users:
+    username = user['username']
+    if user['email'] == '': email = 'admin@eic.network'
+    else: email = user['email']
+    passw = generate_password(15)
+
+    if username in users_in_redis:
+        pass
+    else:
+        subprocess.call(["/var/local/hypothesis/bin/run-h-add-user",
+                        username, email, passw])
